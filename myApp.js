98308449f6e1ca2afb64de4c86edbdb534a049bb
repl.it user <replituser@example.com>
2021-03-12 23:27:37 +0000

    finish

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 console.log('Hello World');
 
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/",function(req, res) {
@@ -27,6 +29,25 @@ app.get('/now', function(req, res, next) {
 }, function(req, res) {
   res.send({time : req.time});
 });
+
+app.get("/:word/echo",function(req, res) {
+  
+  res.json({ echo : req.params.word});
+
+});
+
+app.route("/name")
+  .get(function(req, res) {
+
+    console.log(req.query);
+    res.json({ name : req.query.first + ' ' + req.query.last });
+
+  })
+  .post(function(req, res) {
+    console.log(req.body);
+    res.json({ name : req.body.first + ' ' + req.body.last });
+
+  });
 
 
 
